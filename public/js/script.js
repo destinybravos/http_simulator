0d36fdb223726c24fa3f025e201f293896c94d6d
Silver-Splash <silveriwuji91@gmail.com>
2020-09-21 20:36:50 +0100

    Added Some UI (Tabs and Dropdown)

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,13 @@
+$('#tabs li a').on('click', function () {
+    // Get the Clicked Tab data-target (as the ID Content)
+    $tab = $(this).data('target');
 
+    // Remove Current Active and Place in the new one
+    $('.tab-contents').removeClass('active');
+    $($tab).addClass('active');
+
+    // Change the Active Tab
+    $('#tabs li a').removeClass('border-l border-t border-r');
+    $(this).addClass('border-l border-t border-r')
+
+});
